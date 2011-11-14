11ddaeb77fe2dd1f8bb74d997b40d7841bb44c0b
Stephen Dolier <stephen@dolier.net>
2011-11-14 17:36:37 +1100

    Added Sample project

---
+++ Sample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLog;
+
+namespace Sample
+{
+    class Program
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            Logger.Debug("Debug message 1");
+            Logger.Debug("Debug message 2");
+            Logger.Error("Error message 1");
+        }
+    }
+}
